b8e13ee7778c87cb5ca6f0674a21b3eedfed75b0
minhtri <dotranminhtri12a11999@gmail.com>
2019-09-28 15:18:11 +0700

    minhtrideptrai

---
+++ Tuan3/lap1/src/JFrame_caban.java
@@ -0,0 +1,26 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MINHTRILATAO
+ */
+public class JFrame_caban extends JFrame{
+   public JFrame_caban(){
+       setVisible(true);
+       
+   }
+    public static void main(String[] args) {
+        new JFrame_canban();
+    }
+
+
+    
+    
+}
